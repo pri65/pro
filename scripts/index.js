1854b080a04783d6278c134041648016901cf534
pri65 <priyankasoniii65@gmail.com>
2022-06-18 13:06:21 +0530

    new

--- scripts/index.js
+++ scripts/index.js
@@ -15,3 +15,25 @@
 
 
 // document.getElementsByClassName("productName").innerText = data.productName;
+
+
+let currData = {
+    productName:
+      "Cordless Tire Inflator Portable Compressor Digital Car Tire Pump 12V 150PSI Rechargeable Air Pump -Black",
+    currencySign: "$",
+    offerPrice: "3690.43",
+    actualPrice: "4336.41",
+    discount: "(15% off)",
+    shipToOmit: ["Russia", "Arab"],
+    imageUrl:
+      "https://img.gkbcdn.com/p/2020-08-03/Car-Tyre-Pump-12V-150PSI-Rechargeable---Black-425899-0._w280_p1_.jpg",
+    review_num: "9",
+    freeShip: true,
+    productId: 1234,
+    shipto: "Armenia",
+  };
+  
+  // CurrentData get key=>
+  let materSingleData =
+    JSON.parse(localStorage.getItem("dataToLocalDetail")) || {}; // object
+  // CurrentData get key=>
