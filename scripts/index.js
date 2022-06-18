1e96cc2b953d05cb84558273d1bd6863209e9366
pri65 <priyankasoniii65@gmail.com>
2022-06-18 13:05:34 +0530

    new

--- scripts/index.js
+++ scripts/index.js
@@ -1,17 +1,17 @@
-let product = {
-    "productName": "cycle",
-    "offerPrice": "210",
-    "actualPrice": "300",
-    "discount": "90",
-    "shipForm": "xyz",
-    "shipToOmit": ["india","USA"],
-    "imageUrl": "/jksdhfkjdlfkzd"
-}
+// let product = {
+//     "productName": "cycle",
+//     "offerPrice": "210",
+//     "actualPrice": "300",
+//     "discount": "90",
+//     "shipForm": "xyz",
+//     "shipToOmit": ["india","USA"],
+//     "imageUrl": "/jksdhfkjdlfkzd"
+// }
 
-localStorage.setItem("singleProductView", JSON.stringify(product) )
+// localStorage.setItem("singleProductView", JSON.stringify(product) )
 
-let data = JSON.parse(localStorage.getItem("singleProductView"));
-console.log(data)
+// let data = JSON.parse(localStorage.getItem("singleProductView"));
+// console.log(data)
 
 
-document.getElementsByClassName("productName").innerText = data.productName;
+// document.getElementsByClassName("productName").innerText = data.productName;
